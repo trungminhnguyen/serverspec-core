ba76a7da3e61562385a188b1cd09431dec13f086
Yury Tsarev <yury.tsarev@gooddata.com>
2014-06-12 12:57:02 +0200

    REFACTOR: PCI-3867 fix styling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'serverspec'
 require 'pathname'
 require 'net/ssh'
@@ -8,15 +9,15 @@ include Serverspec::Helper::DetectOS
 
 RSpec.configure do |c|
   c.disable_sudo = true
-  c.path  = "/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin"
+  c.path  = '/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin'
   c.host  = ENV['TARGET_HOST']
   options = Net::SSH::Config.for(c.host)
   user    = options[:user] || Etc.getlogin
   c.ssh   = Net::SSH.start(c.host, user, options)
   c.os    = backend.check_os
 
-  tags = (ENV['TARGET_TAGS'] || "").split(",")
-  c.filter_run_excluding :tag => lambda { |t|
-    not tags.include?(t)
-  }
+  tags = (ENV['TARGET_TAGS'] || '').split(',')
+  c.filter_run_excluding tag: lambda do |t|
+    !tags.include?(t)
+  end
 end
