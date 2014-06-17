7ead34c52afbdd097e1f149b288a6e3a2ceca2a3
Yury Tsarev <yury.tsarev@gooddata.com>
2014-06-17 13:09:41 +0200

    FEATURE: PCI-3867 Encryption test

--- spec/all/kickstart_spec.rb
+++ spec/all/kickstart_spec.rb
@@ -374,3 +374,28 @@ end
 describe file('/proc/cmdline') do
   its(:content) { should match /elevator=deadline intremap=no_x2apic_optout nomodeset video=efifb/ }
 end
+
+describe 'encryption', tag: 'crypt' do
+  it 'should have 256 keysize' do
+    keysizes = command("ls -1 /dev/mapper/luks-* | xargs -n1 cryptsetup status|grep keysize| awk '{print $2}'").stdout
+    keysizes.should_not be_empty
+    keysizes.split.each do |keysize| 
+      keysize.to_i.should eq 256
+    end
+  end
+
+  describe file('/boot') do
+    it { should be_mounted.with( :type => 'ext4' ) }
+  end
+  describe file('/boot/efi') do
+    it { should be_mounted.with( :type => 'vfat' ) }
+  end
+
+  it 'has all physical partitions except /boot and /boot/efi are encrypted' do
+    crypts = command("blkid | grep \/sd|tail -n +3|awk '{print $3}'").stdout
+    crypts.split.length.should > 0
+    crypts.split.each do |type|
+      type.should == 'TYPE="crypto_LUKS"'
+    end
+  end
+end
