defa2c7b4afe820a3597e29aee69ec13fc18d88c
Martin Surovcak <martin.surovcak@gooddata.com>
2014-07-04 11:09:11 +0200

    TEST: Fix git command

--- spec/all/puppet_cloud_spec.rb
+++ spec/all/puppet_cloud_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 # Check if we're on origin and master
-describe command('cd /etc/puppet && git branch -vv') do
-  it { should match Regexp.new('^\* master\s+[0-9a-z]+ \[origin/master\]') }
+describe command('git --git-dir=/etc/puppet/.git/ branch -vv') do
+  its(:stdout) { should match Regexp.new('^\* master\s+[0-9a-z]+ \[origin/master\].*') }
 end
 
 # Check if origin is correct one
-describe command('cd /etc/puppet && git remote -v') do
-  it {should match Regexp.new('^origin\s+git@github.com:gooddata/puppet-cloud.git') }
+describe command('git --git-dir=/etc/puppet/.git/ remote -v') do
+  its(:stdout) {should match Regexp.new('^origin\s+git@github.com:gooddata/puppet-cloud.git.*') }
 end
