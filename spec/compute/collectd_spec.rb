757219f62e150a70894f0904da344afe8e68a4e6
Martin Surovcak <martin.surovcak@gooddata.com>
2014-07-14 11:33:45 +0200

    TEST: PCI-3974 Enabling of collectd + connectivity test

--- spec/compute/collectd_spec.rb
+++ spec/compute/collectd_spec.rb
@@ -1,4 +1,10 @@
+require 'spec_helper'
+
 describe service('collectd') do
-  it { should_not be_enabled }
-  it { should_not be_running }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe host('carbon-proxy-internal.na.intgdc.com') do
+  it { should be_reachable.with( :port => 2003 ) }
 end
