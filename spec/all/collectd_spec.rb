48884e7b68d34945d66ef5185610807d9e629f35
Yury Tsarev <yury.tsarev@gooddata.com>
2014-07-21 17:05:36 +0200

    TEST: PCI-3977 check net netlink/interface configuration

---
+++ spec/all/collectd_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe service('collectd') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe host('carbon-proxy-internal.na.intgdc.com') do
+  it { should be_reachable.with( :port => 2003 ) }
+end
+
+describe "PCI-3977" do
+  netlink = 'LoadPlugin netlink
+<Plugin netlink>
+  Interface "All"
+  VerboseInterface "lo"
+  QDisc "lo"
+  IgnoreSelected yes
+</Plugin>'
+
+  describe file('/etc/collectd.d/netlink.conf') do
+    its(:content) { should match Regexp.new(netlink) }
+  end
+
+  common = 'LoadPlugin interface
+<Plugin interface>
+    Interface "lo"
+    IgnoreSelected true
+</Plugin>'
+
+  describe file('/etc/collectd.d/common.conf') do
+    its(:content) { should match Regexp.new(common) }
+  end
+end
