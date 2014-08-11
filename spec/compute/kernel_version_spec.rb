584f61fb8647cd808cc9e9558349440ae9d8f7a0
Yury Tsarev <yury.tsarev@gooddata.com>
2014-08-11 12:27:25 +0200

    TEST: PCI-4028 check latest available kernel version

--- spec/compute/kernel_version_spec.rb
+++ spec/compute/kernel_version_spec.rb
@@ -8,3 +8,7 @@ end
 describe command('uname -r') do
   its(:stdout) { should < '3.12.23' }
 end
+
+describe command('yum --showduplicates list available kernel-lt|awk \'END{print $2}\'') do
+  its(:stdout) { should < '3.12.23' }
+end
