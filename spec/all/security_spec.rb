a6f507af8b7fd6a1607ca60e26e28c9b2b568258
Yury Tsarev <yury.tsarev@gooddata.com>
2014-09-25 15:52:43 +0200

    TEST: PCI-4181 test bash version for CVE-2014-6271

---
+++ spec/all/security_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+# http://lwn.net/Articles/613032/
+# using command as serverspec verision method does not include release
+describe command('rpm -q --queryformat "%{version}.%{release}" bash') do
+  its(:stdout) { should >= '4.1.2-15.el6_5.1' }
+end
