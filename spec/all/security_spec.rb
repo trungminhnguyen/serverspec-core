f7f7ed955df78f9d687f95efccfa463e9b2e2a86
Yury Tsarev <yury.tsarev@gooddata.com>
2014-09-26 20:38:57 +0200

    TEST: PCI-4183 bash CVE-2014-7169 fix

--- spec/all/security_spec.rb
+++ spec/all/security_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 # http://lwn.net/Articles/613032/
 # using command as serverspec verision method does not include release
-describe command('rpm -q --queryformat "%{version}.%{release}" bash') do
-  its(:stdout) { should >= '4.1.2-15.el6_5.1' }
+describe command('rpm -q --queryformat "%{version}-%{release}" bash') do
+  its(:stdout) { should eq '4.1.2-15.el6_5.2' }
 end
