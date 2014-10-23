8af152c64a15a06692f857d91c6aa24bf251e1cc
Yury Tsarev <yury.tsarev@gooddata.com>
2014-10-23 16:32:18 +0200

    TEST: PCI-3889 PCI-4223 DDT-734 DDT-683 DDT-915 strict repository check
    Recently we faced severe changes in repository layout
    with introduction of pulp and old repo deprecation.
    
    Now it's time to introduce strict check of enabled yum repos
    with proper url correctness test

---
+++ spec/all/repos_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+# Checking url correctness for all enabled repos. 
+# We are intentionally ignoring dell's omsa repo
+
+excluded_repos = 'dell\|folsom' # the ones that we don't check
+disabled_repos = 'gdc-tools-testing' # the ones we check for url but they should be disabled
+
+describe command("yum repolist enabled --verbose| grep baseurl | sort | grep -v '#{excluded_repos}'") do
+  its(:stdout) { should eq "Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/cure-puppet/
+Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/epel/6/x86_64/
+Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/gdc_tools/centos/6/x86_64/
+Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/sl/6.5/x86_64/os/
+Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/sl/6.5/x86_64/security/
+" }
+end
+
+describe command("yum repolist disabled --verbose | grep baseurl | sort | grep '#{disabled_repos}'") do
+  its(:stdout) { should eq "Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/gdc-tools-testing/centos/6/x86_64/
+" }
+end
