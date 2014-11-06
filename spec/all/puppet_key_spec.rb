b572e0409edbcc8dcbc5c097f3a2c3c7da115c2a
Martin Surovcak <martin.surovcak@gooddata.com>
2014-11-06 12:52:56 +0100

    TEST: SEC-704 Rotate puppet_master key

--- spec/all/puppet_key_spec.rb
+++ spec/all/puppet_key_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe file('/root/.ssh/puppet_master') do
-  it { should match_sha256checksum '89fc621505e844b99bfb14f7856b1119bee61dbbec202e346c78a160dda65482' }
+  it { should match_sha256checksum '8035ed1bfa10fad13cef151e626c70524c8aaba5ec65646d1dcda11a1990a7cc' }
 end
 
 # connecting to github
