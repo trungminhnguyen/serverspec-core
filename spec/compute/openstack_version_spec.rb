4290ab550589e08e2259db6f199588b3b92e799b
Yury Tsarev <yury.tsarev@gooddata.com>
2014-11-21 13:21:30 +0100

    TEST: PCI-4258 bump openstack-nova version

--- spec/compute/openstack_version_spec.rb
+++ spec/compute/openstack_version_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe package('openstack-nova') do
-  it { should be_installed.with_version('2012.2.4-35.el6') }
+  it { should be_installed.with_version('2012.2.4-37.el6') }
 end
