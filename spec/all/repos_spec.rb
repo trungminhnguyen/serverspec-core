5f15cf63a526ee02391e16c0d615b0580a920378
Yury Tsarev <yury.tsarev@gooddata.com>
2014-11-21 12:41:14 +0100

    TEST: PCI-4258 update repository layout
    * Fix yumrepo sorted list
    * gdc-tools-testing => tools/testing

--- spec/all/repos_spec.rb
+++ spec/all/repos_spec.rb
@@ -4,14 +4,14 @@ require 'spec_helper'
 # We are intentionally ignoring dell's omsa repo
 
 excluded_repos = 'dell\|folsom' # the ones that we don't check
-disabled_repos = 'gdc-tools-testing' # the ones we check for url but they should be disabled
+disabled_repos = 'tools/testing' # the ones we check for url but they should be disabled
 
 describe command("yum repolist enabled --verbose| grep baseurl | sort | grep -v '#{excluded_repos}' | sed 's:/$::g'") do
   its(:stdout) { should eq "Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/cure-puppet
 Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/epel/6/x86_64
-Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/tools/develop/centos/6/x86_64
 Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/sl/#{os[:release]}/x86_64/os
 Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/sl/#{os[:release]}/x86_64/security
+Repo-baseurl : http://mgmt-repo02.int.na.getgooddata.com:8080/pulp/repos/tools/develop/centos/6/x86_64
 " }
 end
 
