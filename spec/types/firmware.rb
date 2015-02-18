18b863b18cd938120a5bdaab76dd2062fdb1b55c
Yury Tsarev <yury.tsarev@gooddata.com>
2015-02-18 17:02:50 +0100

    FEATURE: PCI-4501 enhance and refactor firmware spec
    * New firmware resource type
    * Check only active nics not depending on actual index order
    * Cleaner spec using resource type abstraction

---
+++ spec/types/firmware.rb
@@ -0,0 +1,47 @@
+module Serverspec
+  module Type
+
+    class Firmware < Base
+
+      def bios
+        version_fetch('BIOS')
+      end
+
+      def idrac
+        version_fetch('iDRAC7')
+      end
+
+      def lifecyclecontroller
+        version_fetch('Lifecycle Controller')
+      end
+
+      def raid
+        version_fetch('PERC H710')
+      end
+
+      def nics
+        active_nics = backend.run_command("omreport chassis nics |grep -B 4 'Connection Status : Connected'|awk '/Index/ {print $3}'")
+        active_nics = active_nics.stdout.split
+        nic_versions = []
+        active_nics.each do |index|
+          version = backend.run_command("omreport chassis nics index=#{index} | grep Firmware -A1 | tail -1 ")
+          nic_versions << version.stdout.chomp
+        end
+        nic_versions
+      end
+
+      private
+
+      def version_fetch(thing)
+        ret = backend.run_command "omreport system version | grep '#{thing}' -A1|tail -1 | awk '{print $3}'"
+        ret.stdout.chomp
+      end
+
+    end
+
+    def firmware
+      Firmware.new
+    end
+
+  end
+end
