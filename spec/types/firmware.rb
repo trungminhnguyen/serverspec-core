8582d57e6d15d74289ea20d477fd11502dc2eaf7
Yury Tsarev <yury.tsarev@gooddata.com>
2015-04-09 15:28:10 +0200

    TEST: PCI-4534 test for H710P raid model
    * Extend firmware custom resource type with
      raid model detection
    * Expect H710P on all compute nodes

--- spec/types/firmware.rb
+++ spec/types/firmware.rb
@@ -19,6 +19,11 @@ module Serverspec
         version_fetch('PERC H710')
       end
 
+      def raid_model
+        ret = @runner.run_command "omreport system version | awk '/PERC/ {print $4}'"
+        ret.stdout.chomp
+      end
+
       def nics
         active_nics = @runner.run_command("omreport chassis nics |grep -B 4 'Connection Status : Connected'|awk '/Index/ {print $3}'")
         active_nics = active_nics.stdout.split
