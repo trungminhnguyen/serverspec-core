6f40042fe682e509a2b2b02b3375f55174f13c25
Martin Surovcak <martin.surovcak@gooddata.com>
2015-07-09 06:19:24 +0200

    CONFIG: PCI-4880 get_all_hosts repects CONF_DIR settings

--- cfg/cfg_helper.rb
+++ cfg/cfg_helper.rb
@@ -6,11 +6,13 @@ def get_environment
 end
 
 def get_main_config(conf_dir='./cfg/')
+  conf_dir = get_config_option('CONF_DIR', conf_dir)
   return YAML.load_file "#{conf_dir}/serverspec.yml"
 end
 
 def get_all_hosts(conf_dir='./cfg/')
   env = get_environment()
+  conf_dir = get_config_option('CONF_DIR', conf_dir)
   config = get_main_config(conf_dir)
   @hosts = File.join(conf_dir, config[env][:hosts])
   return YAML.load_file(ENV['HOSTS'] || @hosts)
